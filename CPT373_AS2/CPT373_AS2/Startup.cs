be637e70ecc647072bfdfe58893134cd36238008
Nathan Waters <nathanpwaters@outlook.com>
2016-07-18 20:37:14 +1000

    -new project using MVC template including WebForms structure

---
+++ CPT373_AS2/CPT373_AS2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CPT373_AS2.Startup))]
+namespace CPT373_AS2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
