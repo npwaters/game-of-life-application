026d46b0426d2133767583f3909a308d19763fe6
Nathan Waters <nathanpwaters@outlook.com>
2016-08-09 08:12:02 +1000

    -onedrive conflict

---
+++ CPT373_AS2/CPT373_AS2/Hubs/GolHub-DESKTOP-8MKHHD4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace CPT373_AS2
+{
+    public class GolHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+
+        // TODO:
+        // implement all playgame code in the Hub??
+
+
+        public void PlayActiveGame(string cells)
+        {
+            Clients.Caller.addTurnToPage(cells);
+        }
+    }
+}
