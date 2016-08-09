3a379562821245664e76b77330d0a7d3fc6e9986
Nathan Waters <nathanpwaters@outlook.com>
2016-08-09 22:06:01 +1000

    -working on sending back the cells string from the hub

--- CPT373_AS2/CPT373_AS2/Hubs/GOLHub.cs
+++ CPT373_AS2/CPT373_AS2/Hubs/GOLHub.cs
@@ -5,6 +5,9 @@ using System.Web;
 using Microsoft.AspNet.SignalR;
 
 using CPT373_AS2.Models;
+using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace CPT373_AS2
 {
@@ -22,7 +25,27 @@ namespace CPT373_AS2
 
         public void PlayActiveGame(UserGame game)
         {
-            //Clients.Caller.addTurnToPage(cells);
+            StringBuilder output = new StringBuilder();
+            output.Append("\u0020");
+            output.Append("\u0020");
+            output.Append("\u0020");
+            output.AppendLine();
+            output.Append('\u2588');
+            output.Append('\u2588');
+            output.Append('\u2588');
+            output.AppendLine();
+            output.Append("\u0020");
+            output.Append("\u0020");
+            output.Append("\u0020");
+
+
+            game.Cells = "XXX\r\nOOO\r\nXXX";
+
+            //Clients.Caller.addTurnToPage(JsonConvert.SerializeObject(game));
+            MvcHtmlString cells = new MvcHtmlString(output.ToString());
+
+            Clients.Caller.addTurnToPage(cells);
+            //Clients.Caller.addTurnToPage(JsonConvert.SerializeObject(cells));
         }
     }
 }
