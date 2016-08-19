5d5e4e90c4fed184094de1cda0eee3c518818954
Nathan Waters <nathanpwaters@outlook.com>
2016-08-19 13:48:42 +1000

    -WebForm retrieving user list

---
+++ CPT373_AS2/CPT373_AS2/UserList.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CPT373_AS2
+{
+    public partial class UserList1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UsersManagerDisconnected usersManager = new UsersManagerDisconnected();
+            DataTable usersTable = usersManager.GetUsers();
+
+            UserListGridView.DataSource = usersTable;
+            UserListGridView.DataBind();
+        }
+    }
+}
---
+++ CPT373_AS2/CPT373_AS2/UsersManagerDisconnected.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CPT373_AS2
+{
+    public class UsersManagerDisconnected
+    {
+        public DataTable GetUsers()
+        {
+
+
+
+
+            using (var connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\GOLDB.mdf;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework"))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = "select [UserID],[Email],[Password],[FirstName],[LastName] from [User]";
+
+                var dataTable = new DataTable();
+
+                var adapter = new SqlDataAdapter(command);
+                adapter.Fill(dataTable);
+
+                return dataTable;
+            }
+
+        }
+    }
+
+
+}
